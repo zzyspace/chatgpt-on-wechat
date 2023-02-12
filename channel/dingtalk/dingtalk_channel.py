f5da5f8990eb9a466be15f85da5a9cd017b477ac
Ryan Zhang <551854173@qq.com>
2023-02-12 21:49:24 +0800

    feat: ding access token

--- channel/dingtalk/dingtalk_channel.py
+++ channel/dingtalk/dingtalk_channel.py
@@ -9,16 +9,20 @@ import requests
 import hmac  
 import hashlib  
 import base64 
+import io
+import ding_access_token
 from common.log import logger
 from concurrent.futures import ThreadPoolExecutor
 from channel.dingtalk.tornado_utils import Application, route
 from channel.channel import Channel
 from config import conf, dynamic_conf
+from ding_access_token import AccessToken
 
 # thread_pool = ThreadPoolExecutor(max_workers=8)
 
 @route("/")
 class DingtalkChannel(tornado.web.RequestHandler, Channel):
+    _access_token = ''
     #def __init__(self):
     #    pass
 
@@ -34,25 +38,29 @@ class DingtalkChannel(tornado.web.RequestHandler, Channel):
         hmac_code = hmac.new(app_secret_enc, string_to_sign_enc, digestmod=hashlib.sha256).digest()  
         my_sign = base64.b64encode(hmac_code).decode('utf-8')  
         if sign != my_sign:  
-            return self.finish({"errcode":1,"msg":"签名有误"})  
+            return self.finish({"errcode":1,"msg":"wrong signature"})  
 
         data = json.loads(self.request.body)
         self.handle(data)
         ret = {"errcode":0,"msg":'success'}
         return self.finish(ret)
-        # return self.write_json(ret)
-
-    def write_json(self, struct):
-        self.set_header("Content-Type", "application/json; charset=UTF-8")
-        self.write(tornado.escape.json_encode(struct))
 
     def push_ding(self, msg, uid):
-        
         try:
             app_key = dynamic_conf()['global']['ding_app_key']
             resp = requests.post("https://api.dingtalk.com/v1.0/robot/oToMessages/batchSend",
             data=json.dumps({"robotCode":app_key,"userIds":[uid],"msgKey":"sampleText","msgParam":'{"content":"'+msg+'"}'}),
-            headers={"Content-Type":"application/json","x-acs-dingtalk-access-token":'9ec6668c191c32d490c5b1d5dfab3412'})
+            headers={"Content-Type":"application/json","x-acs-dingtalk-access-token":DingtalkChannel._access_token.get_access_token()})
+            logger.info(f'[Ding] push_ding response: {resp}')
+        except Exception as e:
+            logger.error(e)
+
+    def push_img_ding(self, img, uid):
+        try:
+            app_key = dynamic_conf()['global']['ding_app_key']
+            resp = requests.post("https://api.dingtalk.com/v1.0/robot/oToMessages/batchSend",
+            data=json.dumps({"robotCode":app_key,"userIds":[uid],"msgKey":"sampleImageMsg","msgParam":'{"photoURL":"'+img+'"}'}),
+            headers={"Content-Type":"application/json","x-acs-dingtalk-access-token":DingtalkChannel._access_token.get_access_token()})
             logger.info(f'[Ding] push_ding response: {resp}')
         except Exception as e:
             logger.error(e)
@@ -60,7 +68,7 @@ class DingtalkChannel(tornado.web.RequestHandler, Channel):
     # Channel
 
     @classmethod
-    def startup(self):
+    def startup(cls):
         # 运行服务
         loop = asyncio.get_event_loop()
         http_server = tornado.httpserver.HTTPServer(Application())
@@ -69,6 +77,9 @@ class DingtalkChannel(tornado.web.RequestHandler, Channel):
 
         loop.run_forever()
 
+        # 初始化 access token
+        cls._access_token = AccessToken()
+
     def handle(self, msg):
         content = msg['text']['content']
         nickname = msg['senderNick']
@@ -76,7 +87,7 @@ class DingtalkChannel(tornado.web.RequestHandler, Channel):
         logger.debug(f"[Ding]receive msg: {json.dumps(msg, ensure_ascii=False)}\nuser: {nickname}\nid: {from_user_id}")
 
         match_prefix = self.check_prefix(content, conf().get('single_chat_prefix'))
-        match_payment = True #self.check_payment(nickname) # TODO
+        match_payment = self.check_payment(nickname)
         logger.debug(f'[Ding] match: {match_prefix is not None}, {match_payment}')
         if match_payment and match_prefix is not None:
             if match_prefix != '':
@@ -96,7 +107,7 @@ class DingtalkChannel(tornado.web.RequestHandler, Channel):
 
     def send(self, msg, receiver):
         logger.info('[WX] sendMsg={}, receiver={}'.format(msg, receiver))
-        self.push_ding(msg)
+        self.push_ding(msg, receiver)
 
     def _do_send(self, query, reply_user_id):
         try:
@@ -107,6 +118,22 @@ class DingtalkChannel(tornado.web.RequestHandler, Channel):
             reply_text = super().build_reply_content(query, context)
             if reply_text:
                 self.send(conf().get("single_chat_reply_prefix") + reply_text, reply_user_id)
+        except Exception as e:
+            logger.exception(e)
+
+    def _do_send_img(self, query, reply_user_id):
+        try:
+            if not query:
+                return
+            context = dict()
+            context['type'] = 'IMAGE_CREATE'
+            img_url = super().build_reply_content(query, context)
+            if not img_url:
+                return
+
+            # 图片发送
+            logger.info('[WX] sendImage, receiver={}'.format(reply_user_id))
+            self.push_img_ding(img_url, reply_user_id)
         except Exception as e:
             logger.exception(e)
     
