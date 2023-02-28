68b3f0d9dc0b8e952a9f7803c725adfa44d38c68
Ryan Zhang <551854173@qq.com>
2023-02-28 18:42:29 +0800

    fix: reply with dynamic config

--- auto_reply/reply.py
+++ auto_reply/reply.py
@@ -3,7 +3,6 @@ from config import dynamic_conf
 from payment.payment import Payment
 from common import const
 
-_config = dynamic_conf()['auto_reply']
 _cmds = [
     '/info',
     '/help',
@@ -16,28 +15,28 @@ class Reply(object):
         return content in _cmds
 
     def reply_newbie(self):
-        return _config['newbie'].replace('\\n', '\n') + self.reply_help()
+        return self._config()['newbie'].replace('\\n', '\n') + self.reply_help()
 
     def reply_help(self):
-        return _config['help'].replace('\\n', '\n')
+        return self._config()['help'].replace('\\n', '\n')
 
     def reply_runout(self):
-        return _config['runout'].replace('\\n', '\n')
+        return self._config()['runout'].replace('\\n', '\n')
     
     def reply_bound_code(self, user_id, nickname):
         payment = Payment()
         amount = payment.get_amount(user_id, nickname)
-        bound_text = _config['bound']
+        bound_text = self._config()['bound']
         return f'{bound_text} 剩余额度: {amount}次'
 
     def reply_bound_invalid(self, user_id, nickname):
-        return _config['bound_invalid'].replace('\\n', '\n')
+        return self._config()['bound_invalid'].replace('\\n', '\n')
 
     def reply_bound_referral(self):
-        return _config['bound']
+        return self._config()['bound']
 
     def reply_bound_referral_invalid(self):
-        return _config['bound_referral_invalid']
+        return self._config()['bound_referral_invalid']
 
     def reply_with(self, user_id, nickname, content):
         if self.is_auto_reply(content):
@@ -50,5 +49,8 @@ class Reply(object):
                 return f'ID: {user_id}\n用户名: {nickname}\n推荐码: {referral}\n\n:剩余额度: {amount}次\n卡号: {code}'
             elif content == '/help':
                 return self.reply_help()
+
+    def _config(self):
+        return dynamic_conf()['auto_reply']
                 
 
