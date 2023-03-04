5f187ea512d3252dde1a4be9515dc3166f332ba9
Ryan Zhang <551854173@qq.com>
2023-03-04 18:26:19 +0800

    fix bug

--- channel/wechat/wechat_mp_service_channel.py
+++ channel/wechat/wechat_mp_service_channel.py
@@ -154,9 +154,9 @@ class WechatMPServiceChannel(Channel):
 
         # 新人
         if self._payment.is_newbie(user_id, nickname):
-            pass
-            # reply = self._reply.reply_newbie(user_id)
-            # self.send(reply, user_id)
+            # pass
+            reply = self._reply.reply_newbie(user_id)
+            self.send(reply, user_id)
             # self._fetch_user_info(user_id)
         # 敏感词
         if sensitive_detector.detect(content):
